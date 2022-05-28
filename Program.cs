222d6f07d40252b773f28bb3f93ef8f1b5f5067e
marius.herciu23 <marius.herciu23@gmail.com>
2022-05-28 10:21:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex4_Laborator4_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex 4
+            //Scrieti o functie recursiva care va afisa in ordine elementele unui vector de
+            //intregi
+            int[] vector = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int n = 0;
+            AfisareVector(vector, n);
+        }
+        static void AfisareVector(int[] vector, int n)
+        {
+            while (n >= vector.Length)
+            {
+                return;
+            }
+            Console.Write(vector[n] + " ");
+            AfisareVector(vector, n + 1);
+        }
+    }
+}
